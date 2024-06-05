9a3941bf78b0bd3671c7c239a0b5a01dc75724c4
fridapfrandsen <159155878+fridapfrandsen@users.noreply.github.com>
2024-06-05 15:21:19 +0200

    With different tests

--- Hamming Codes/HammingCodeTest.py
+++ Hamming Codes/HammingCodeTest.py
@@ -1,7 +1,7 @@
 from HammingCodeSevenFour import *
 
 hamming74 = HammingCodeSevenFour()
-message = "0011"
+message = "1011"
 encoded = hamming74.Encode(message)
 codeword_with_error = hamming74.MakeError(encoded)
 decoded = hamming74.Decode(codeword_with_error)
