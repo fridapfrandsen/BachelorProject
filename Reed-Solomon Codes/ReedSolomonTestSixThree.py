c744d631a3db0db9da9a4bdae26c60228ab38b5c
fridapfrandsen <159155878+fridapfrandsen@users.noreply.github.com>
2024-06-06 15:25:37 +0200

    Final Version

--- Reed-Solomon Codes/ReedSolomonTestSixThree.py
+++ Reed-Solomon Codes/ReedSolomonTestSixThree.py
@@ -1,7 +1,7 @@
 from ReedSolomonPrimeField import *
 
 RS_6_3 = ReedSolomonPrimeField(6, 3, 3)
-message = [1, 0, 1]
+message = [4, 1, 2]
 encoded_message = RS_6_3.EncodeMessage(message)
 error_codeword = RS_6_3.MakeError(encoded_message)
 decoded_message = RS_6_3.DecodeMessage(error_codeword)
